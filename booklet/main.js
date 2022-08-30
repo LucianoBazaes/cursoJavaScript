e8871f4e98e7524fffd78cad4781c651c71f6801
LucianoBazaes <lucho.bazaes@gmail.com>
2022-08-29 22:28:46 -0300

    ejercicios clase 1

---
+++ booklet/main.js
@@ -0,0 +1,56 @@
+//EJERCICIOS DE LA CLASE NÚMERO 1
+
+/* EJERCICIO "HOMERO"
+let nombre = "Homero";
+let apellido = "Simpson";
+let edad = 38; */
+
+/*EJERCICIO "CIUDADES"
+const CIUDAD1 = "Mendoza";
+const CIUDAD2 = "La Plata";
+const CIUDAD3 = "Paris";
+const CIUDAD4 = "Berlin";
+const CIUDAD5 = "Madrid"; */
+
+/*EJERCICIO "CARNET"
+let nombrePersona = "Bart";
+let apellidoPersona = "Simpson";
+let fechaNacimiento = "2 de Noviembre de 1970";
+let edadPersona = 10;
+let direccionCasa = "Avenida Siempre Viva 742";
+let ciudadPersona = "Springfield";
+let sexoPersona = "Masculino";
+
+let carnet = "Nombre: " + nombrePersona + "" +
+             " Apellido: " + apellidoPersona + "" +
+             " Fecha de Nacimiento: " + fechaNacimiento + "" + 
+             " Edad: " + edadPersona + "" +
+             " Direccion: " + direccionCasa + "" +
+             " Ciudad: " + ciudadPersona + "" +
+             " Sexo: " + sexoPersona;
+
+console.log(carnet); */
+
+/* EJERCICIO "REGISTRO DE PERSONAS"
+let nombre1 = prompt("Ingrese el nombre de la primer persona");
+let nombre2 = prompt("Ingrese el nombre de la segunda persona");
+let nombre3 = prompt("Ingrese el nombre de la tercer persona");
+let nombre4 = prompt("Ingrese el nombre de la cuarta persona");
+let nombre5 = prompt("Ingrese el nombre de la quinta persona");
+
+let nombresFamilia = " 1) " + nombre1 + "" +
+                     " 2) " + nombre2 + "" +
+                     " 3) " + nombre3 + "" +
+                     " 4) " + nombre4 + "" +
+                     " 5) " + nombre5;
+alert("Los nombres ingresados son: " + nombresFamilia);*/
+
+/* EJERCICIO "DESCUENTOS"
+let precio = parseFloat(prompt("Ingrese un precio"));
+let descuento10 = precio - (precio * 0.10);
+let descuento40 = precio - (precio * 0.40);
+console.log("Aplicando un 10% de descuento, su precio final es de: $" + descuento10);
+console.log("Aplicando un 40% de descuento, su precio final es de: $" + descuento40); */
+
+//----------------------------------------------------------------------------------------------------------//
+
