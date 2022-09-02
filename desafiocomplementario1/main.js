3e41b8265bf4e4547e0665fb85282e1af4919a97
LucianoBazaes <lucho.bazaes@gmail.com>
2022-09-01 21:39:20 -0300

    Desafio Complementario N°1

--- desafiocomplementario1/main.js
+++ desafiocomplementario1/main.js
@@ -1,13 +1,11 @@
-//El usuario ingresa un numero que representa la cantidad de veces que se va a iterar
 alert("BIENVENIDO");
+//El usuario ingresa un numero que representa la cantidad de veces que se va a iterar
 let numero = parseInt(prompt("Ingrese un número válido"));
-
 
 //Mostramos por consola el número ingresado por el usuario
 console.log("El numero ingresado es: ", numero);
 
-// La variable i inicializa con el valor 0, siempre y cuando sea menor que el numero que ingresa el usuario, mientras sea menor, va a ir iterando
-
+// La variable i inicializa con el valor 0; mientras sea menor que el numero ingresado por el usuario, va a ir iterando
 for (let i = 0; i<numero; i++) {
     //Si el numero es par entonces imprimirá azul proporcionalmente al numero que ingresó el usuario
     if (numero % 2 == 0) {
@@ -18,3 +16,4 @@ for (let i = 0; i<numero; i++) {
         console.log("Rojo");
     }
 }
+alert("Gracias por usar mi programa, vuelva pronto");
